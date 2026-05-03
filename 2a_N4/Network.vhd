25adc69d86d94bdf601ddd25c0648d5474dad23e
Eliott <e.prigent@outlook.fr>
2026-05-03 14:54:41 +0200

    Add VHDL files for Network and Neurone entities, including test scripts and package definitions

---
+++ 2a_N4/Network.vhd
@@ -0,0 +1,55 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+library work;
+use work.pack_neurones.all;
+
+entity Network is
+	Generic (
+		Nb_Layers : natural := 3;
+		Default_Wi : Tab_int_const := (others => 8)
+	);
+	Port(
+		Vect_X : in Tab_int_const;
+		Vect_Z : out Tab_int (1 to (N / (2**(Nb_Layers-1))))
+	);
+end Network;
+
+architecture Structural of Network is
+
+	component Neurone
+		Generic ( Wi : Tab_int_const);
+		Port(
+			Xi : in Tab_int_const;
+			Z : out short_natural
+		);
+	end component;
+
+	type pipe_matrix is array(0 to Nb_Layers) of Tab_int_const;
+	signal pipe : pipe_matrix;
+
+begin
+
+	pipe(0) <= Vect_X;
+	
+	GEN_LAYERS: for i in 0 to Nb_Layers-1 generate
+
+		constant nb_neurones : natural := N / (2**i);
+
+	begin			
+		GEN_NEURONES: for j in 1 to nb_neurones generate
+        	begin
+            		U_NEURONE : Neurone
+                		Generic Map (Wi => Default_Wi)
+                		Port Map (
+                    			Xi => pipe(i),   
+                    			Z  => pipe(i+1)(j)
+                		);
+        	end generate GEN_NEURONES;
+	end generate GEN_LAYERS;
+	
+	GEN_OUT: for k in 1 to (N / (2**(Nb_Layers-1))) generate
+		Vect_Z(k) <= pipe(Nb_Layers)(k);
+	end generate GEN_OUT;
+
+end Structural;
---
+++ 2a_N4/Test_network_NbLayers_3.do
@@ -0,0 +1,32 @@
+# Script .do pour la simulation du Network
+
+# 1. Lancement de la simulation
+vsim work.Test_network_NbLayers_3
+
+configure wave -namecolwidth 120
+configure wave -valuecolwidth 50
+configure wave -justifyvalue left
+configure wave -signalnamewidth 1
+configure wave -snapdistance 10
+configure wave -datasetprefix 0
+configure wave -rowmargin 4
+configure wave -childrowmargin 2
+configure wave -gridoffset 0
+configure wave -gridperiod 1
+configure wave -griddelta 40
+configure wave -timeline 0
+configure wave -timelineunits ns
+
+# 2. Configuration et ouverture de la fenêtre de chronogramme (Wave)
+# Ajoute tous les signaux de l'architecture 'tb_architecture' dans la fenêtre 'wave'
+add wave -divider "Entrees"
+add wave /Test_network_NbLayers_3/Vect_X_in
+
+# Ajoute les signaux de sortie
+add wave -divider "Sorties"
+add wave /Test_network_NbLayers_3/Vect_Z_out
+
+# 3. Lancement de la simulation
+# Exécute la simulation jusqu'à 20 ns pour couvrir tous les stimuli
+run 20 ns
+wave zoomfull
